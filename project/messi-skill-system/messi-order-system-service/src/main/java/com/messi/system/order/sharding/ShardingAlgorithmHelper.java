d017442090ca05db91289ab45abca44a1f18c250
Liu.Qi <123@test.com>
2023-01-24 18:24:53 +0800

    订单系统可正常运行的单库单表环境

--- project/messi-skill-system/messi-order-system-service/src/main/java/com/messi/system/order/sharding/ShardingAlgorithmHelper.java
+++ project/messi-skill-system/messi-order-system-service/src/main/java/com/messi/system/order/sharding/ShardingAlgorithmHelper.java
@@ -1,58 +1,58 @@
-package com.messi.system.order.sharding;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-/**
- * 分片策略算法辅助组件
- */
-@Slf4j
-@Component
-public class ShardingAlgorithmHelper {
-
-    /**
-     * 分4个库
-     */
-    protected static int DATABASE_SIZE = 4;
-
-    /**
-     * 每个库分8张表
-     */
-    protected static int TABLE_SIZE = 8;
-
-//    //  这样的写法，是为了让DATABASE_SIZE和TABLE_SIZE能够传入静态方法中
-//    @Value("${spring.shardingsphere.sub.database.number:4}")
-//    public void setDatabaseSize(int databaseSize) {
-//        DATABASE_SIZE = databaseSize;
+//package com.messi.system.order.sharding;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 分片策略算法辅助组件
+// */
+//@Slf4j
+//@Component
+//public class ShardingAlgorithmHelper {
+//
+//    /**
+//     * 分4个库
+//     */
+//    protected static int DATABASE_SIZE = 4;
+//
+//    /**
+//     * 每个库分8张表
+//     */
+//    protected static int TABLE_SIZE = 8;
+//
+////    //  这样的写法，是为了让DATABASE_SIZE和TABLE_SIZE能够传入静态方法中
+////    @Value("${spring.shardingsphere.sub.database.number:4}")
+////    public void setDatabaseSize(int databaseSize) {
+////        DATABASE_SIZE = databaseSize;
+////    }
+////
+////    @Value("${spring.shardingsphere.sub.table.number:8}")
+////    public void setTableSize(int tableSize) {
+////        TABLE_SIZE = tableSize;
+////    }
+//
+//    /**
+//     * 计算匹配数据源的后缀
+//     * 路由键 对 数据库 做取模
+//     * +"" => 让值转换成string类型
+//     *
+//     * @param valueSuffix 分片键后三位
+//     * @return 数据源后缀
+//     */
+//    public static String getDatabaseSuffix(int valueSuffix) {
+//        return valueSuffix % DATABASE_SIZE + "";
 //    }
 //
-//    @Value("${spring.shardingsphere.sub.table.number:8}")
-//    public void setTableSize(int tableSize) {
-//        TABLE_SIZE = tableSize;
+//    /**
+//     * 计算匹配表的后缀
+//     * 路由键 / 数据库 对表的数量做取模
+//     *
+//     * @param valueSuffix 分片键后三位
+//     * @return 数据源后缀
+//     */
+//    public static String getTableSuffix(int valueSuffix) {
+//        return valueSuffix / DATABASE_SIZE % TABLE_SIZE + "";
 //    }
-
-    /**
-     * 计算匹配数据源的后缀
-     * 路由键 对 数据库 做取模
-     * +"" => 让值转换成string类型
-     *
-     * @param valueSuffix 分片键后三位
-     * @return 数据源后缀
-     */
-    public static String getDatabaseSuffix(int valueSuffix) {
-        return valueSuffix % DATABASE_SIZE + "";
-    }
-
-    /**
-     * 计算匹配表的后缀
-     * 路由键 / 数据库 对表的数量做取模
-     *
-     * @param valueSuffix 分片键后三位
-     * @return 数据源后缀
-     */
-    public static String getTableSuffix(int valueSuffix) {
-        return valueSuffix / DATABASE_SIZE % TABLE_SIZE + "";
-    }
-
-}
+//
+//}
--- project/messi-skill-system/messi-order-system-service/src/main/java/com/messi/system/order/sharding/TablePreciseShardingAlgorithm.java
+++ project/messi-skill-system/messi-order-system-service/src/main/java/com/messi/system/order/sharding/TablePreciseShardingAlgorithm.java
@@ -1,45 +1,45 @@
-package com.messi.system.order.sharding;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
-import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
-
-import java.util.Collection;
-
-/**
- * 分表使用的精准分片算法
- */
-@Slf4j
-public class TablePreciseShardingAlgorithm implements PreciseShardingAlgorithm<String> {
-
-    @Override
-    public String doSharding(Collection<String> tableNames, PreciseShardingValue<String> shardingValue) {
-        return getTableName(tableNames, shardingValue.getValue());
-    }
-
-    /**
-     * 获取表所在的数据源
-     *
-     * @param tableNames  表名
-     * @param columnValue 字段值
-     * @return 匹配的表名
-     */
-    private String getTableName(Collection<String> tableNames, String columnValue) {
-        //  获取业务ID的后三位后缀
-        String valueSuffix = columnValue.length() <= 3 ? columnValue : columnValue.substring(columnValue.length() - 3);
-
-        //  获取将要路由到数据源的后缀
-        String tableSuffix = ShardingAlgorithmHelper.getTableSuffix(Integer.parseInt(valueSuffix));
-
-        for (String tableName : tableNames) {
-            //  返回匹配到的数据源
-            if (tableName.endsWith(tableSuffix)) {
-                return tableName;
-            }
-        }
-
-        log.info("路由键:{}没有精准匹配到表", columnValue);
-        return null;
-    }
-
-}
+//package com.messi.system.order.sharding;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
+//import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
+//
+//import java.util.Collection;
+//
+///**
+// * 分表使用的精准分片算法
+// */
+//@Slf4j
+//public class TablePreciseShardingAlgorithm implements PreciseShardingAlgorithm<String> {
+//
+//    @Override
+//    public String doSharding(Collection<String> tableNames, PreciseShardingValue<String> shardingValue) {
+//        return getTableName(tableNames, shardingValue.getValue());
+//    }
+//
+//    /**
+//     * 获取表所在的数据源
+//     *
+//     * @param tableNames  表名
+//     * @param columnValue 字段值
+//     * @return 匹配的表名
+//     */
+//    private String getTableName(Collection<String> tableNames, String columnValue) {
+//        //  获取业务ID的后三位后缀
+//        String valueSuffix = columnValue.length() <= 3 ? columnValue : columnValue.substring(columnValue.length() - 3);
+//
+//        //  获取将要路由到数据源的后缀
+//        String tableSuffix = ShardingAlgorithmHelper.getTableSuffix(Integer.parseInt(valueSuffix));
+//
+//        for (String tableName : tableNames) {
+//            //  返回匹配到的数据源
+//            if (tableName.endsWith(tableSuffix)) {
+//                return tableName;
+//            }
+//        }
+//
+//        log.info("路由键:{}没有精准匹配到表", columnValue);
+//        return null;
+//    }
+//
+//}
